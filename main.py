bd3b904b3711a31ee8a1270c29c1edb349bc73d8
mdrobot7 <55768773+mdrobot7@users.noreply.github.com>
2022-04-16 19:18:13 -0500

    Update todo, rewrote checkInput, added more functions
    Picking up this project again -- basically just Ctrl-A deleted all of my old code because it was bad and confusing. Rewrote with more functions for clarity and with a better understanding of how worldle works (lol)

--- main.py
+++ main.py
@@ -21,65 +21,60 @@ except FileNotFoundError:
 
 #====================================================================================================================================================================================#
 
+def removeWordsWithLetter(let): #removes words from dict containing the letter 'let'
+    global dict
+    for i in range(len(dict)):
+        if i.find(let) != -1: #if it finds the letter ANYWHERE in the dict word, remove the dict word
+            dict.pop(i)
+            i -= 1
+            
+def removeWordsWithLetterNotInPos(let, pos): #removes words from dict that don't have the letter 'let' in position 'pos'
+    global dict
+    for i in range(len(dict)):
+        if dict[i][pos] != let:
+            dict.pop(i)
+            i -= 1
+
+def pickNextInput(): #selects the next input word
+    #How the algorithm works:
+    #If the solution array is missing more than 2 letters, it picks the word from dict with the most unknown letters
+    #If the solution array is missing 1 or 2 letters, it starts guessing from the available words left
+    #If the solution array is full, it outputs the resulting word.
+
+    global dict
+    global solution
+    global nonLetters
+    pass
+
 def checkInput(_word, _result): #parses the inputted word, and its result. removes bad words from the dictionary list.
     global dict
     global solution
     global nonLetters
 
-    for i in range(5):
-        if _result[i] == "0": #if the i'th character in _word is NOT in the solution word AT ALL
-            count = 0
-            while True:
-                if count >= len(dict):
-                    break
-                if dict[count].find(_word[i]) != -1: #if the current dict word has the bad letter AT ALL, remove the dict word.)
-                    dict.pop(count)
-                    continue
-                count += 1 #only increments the count if it does not pop an index
-        elif _result[i] == "1": #if the i'th letter is correct, but in the WRONG place
-            nonLetters[i] += _word[i]
-            count = 0
-            while True:
-                if count >= len(dict):
-                    break
-                if dict[count].find(_word[i]) == -1: #if the current dict word DOES NOT have the letter, then it will not work -- remove it.
-                    dict.pop(count)
-                    continue
-                count += 1
-        elif _result[i] == "2": #if the i'th letter is correct AND in the right place
-            solution[i] = _word[i] #put the known good letters in the solution string/array
-            count = 0
-            while True:
-                if count >= len(dict):
-                    break
-                if dict[count].find(_word[i]) != i: #if the current dict word has the bad letter in ANY PLACE OTHER THAN THE GOOD ONE, remove the dict word.
-                    dict.pop(count)
-                    continue
-                count += 1
-    """
-    #check everything down here with prints, I'm not sure this algorithm works
-    for a in range(5): #for loop through the nonLetters list
-        if solution[a] != "-": continue
-        for b in range(5): #for loop through the REST of the nonLetters list (skip over nonLetters[a])
-            if b == a: continue
-            if solution[b] != "-": continue
-
-            for c in nonLetters[b]: #for loop through EACH LETTER in nonLetters[b]
-                for d in nonLetters: #for loop through nonLetters again, trying to find nonLetters[b][c]
-                    if d.find(c) == -1: break
-                else:
-                    solution[a] = c #if a particular letter IS in every index other than nonLetters[a], it's a solution
+    for i in range(5): #parse the input, eliminate dict words that CAN'T be the solution
+        if _result[i] == 0:
+            removeWordsWithLetter(_word[i])
+        elif _result[i] == 1:
+            nonLetters[i] = _word[i]
+        elif _result[i] == 2:
+            solution[i] = _word[i]
+            removeWordsWithLetterNotInPos(_word[i], i)
+            if _word[i] == 'q': #handle q-u combinations, just a bit of optimization
+                solution[i + 1] = 'u'
+                removeWordsWithLetterNotInPos('u', i + 1)
     
-    count = 0
-    for i in range(5): #go over the dict again, get rid of any words that don't match the solution string
-        while True:
-            if count >= len(dict):
-                break
-            if dict[count].find(solution[i]) != i: #if the current dict word has the bad letter in ANY PLACE OTHER THAN THE GOOD ONE, remove the dict word.
-                dict.pop(count)
-                continue
-            count += 1"""
-                
+    foundEmptyIndexFlag = False
+    for i in range(5): #check nonLetters for 'process of elimination' cases - where there is only one place a letter can be.
+        for ii in nonLetters[i]:
+            for iii in range(i + 1, 5):
+                if nonLetters[iii].find(ii) == -1 and len(solution[iii]) == 0: foundEmptyIndexFlag = True
+                if nonLetters[iii].find(ii) == -1 and len(solution[iii]) == 0 and foundEmptyIndexFlag: break #break if it finds two indices that could be a possible slot for 'ii'
+            else: #found a slot that works!
+                solution[iii] = ii
+                removeWordsWithLetterNotInPos(ii, iii) #eliminate bad words
+            foundEmptyIndexFlag = False
+    
+    pickNextInput()
 
 #====================================================================================================================================================================================#
 
@@ -101,10 +96,10 @@ while True: #rough cut of the dictionary -- remove bad words, spaces, carriage r
 print("Wordle Solver")
 print("-------------")
 
-word = input("What is your starting word? (Recommended starting word: \'roate\') ")
+word = input("What is your starting word? (Recommended starting word: \'salet\') ")
 while len(word) != 5:
     if len(word) != 5: print("Input words should be 5 letters.")
-    word = input("What is your starting word? (Recommended starting word: \'roate\') ")
+    word = input("What is your starting word? (Recommended starting word: \'salet\') ")
 
 print("Input your starting word into the Wordle website. Enter the result below, in this form: ")
 print("""
